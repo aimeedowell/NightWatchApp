80d9a1f3fd8251c88d69a20aedefb93c6689ab34
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2023-08-31 19:38:12 +0100

    Initial Commit

---
+++ NightWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NightWatch
+//
+//  Created by Aimee Dowell on 31/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Aimee Dowell on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
