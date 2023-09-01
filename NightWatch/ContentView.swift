3c7e98719ba90d646cef5f8997b9fd93ccba5a40
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2023-09-01 17:24:11 +0100

    Version 1

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -9,8 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack (alignment: .leading) {
+            HStack {
+                VStack (alignment: .leading) {
+                    //MARK: Nightly Tasks
+                    Group {
+                    HStack {
+                            Text(Image(systemName: "cloud.moon"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Nightly Tasks")
+                                .underline()
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .padding(.top)
+                            .textCase(.uppercase)
+                        }
+                        Text("- Check all windows")
+                        Text("- Check all doors")
+                        Text("- Check that the safe is locked")
+                        Text("- Check the mailbox")
+                        Text("- Inspect security cameras")
+                        Text("- Clear ice from sidewalks")
+                        Text("- Document \"strange and unusual\" occurences")
+                        Divider()
+                        //MARK: Weekly Tasks
+                        HStack {
+                            Text(Image(systemName: "calendar.day.timeline.left"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Weekly Tasks")
+                                .underline()
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .padding(.top)
+                            .textCase(.uppercase)
+                        }
+                    }
+                    Text("- Check inside all vacant rooms")
+                    Text("- Walk the perimeter of the property")
+                    Divider()
+                    //MARK: Monthly Tasks
+                    HStack {
+                        Text(Image(systemName: "calendar"))
+                            .foregroundColor(.yellow)
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                        
+                        Text("Monthly Tasks")
+                            .underline()
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                            .padding(.top)
+                        .textCase(.uppercase)
+                    }
+                    Text("- Test security alarms")
+                    Text("- Test smoke alarms")
+                }
+                .foregroundColor(.gray)
+                Spacer()
+            }
+            .padding([.top, .leading])
+            Spacer()
+        }
     }
 }
 
