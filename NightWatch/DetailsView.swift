9fdc42e9fb2e69d3f4fb1fbec275a5467bbd2b5f
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2023-09-14 16:20:20 +0100

    Only show task description when phone is landscape

--- NightWatch/DetailsView.swift
+++ NightWatch/DetailsView.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 struct DetailsView: View {
     @Binding var task: Task
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     var body: some View {
         VStack {
+            HStack {
+                Image(systemName: "map")
+                Text("Placeholder for motel floor plan")
+            }
             Text(task.name)
-            Text("Placeholder for task description")
+            
+            if horizontalSizeClass == .regular {
+                Divider()
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                Divider()
+            }
+
             Button("Mark Complete") {
                 task.isComplete = true
             }
@@ -22,6 +33,11 @@ struct DetailsView: View {
 
 struct DetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailsView(task: Binding<Task>.constant(Task(name: "Test Task", isComplete: false, lastCompleted: nil)))
+        Group {
+            DetailsView(task: Binding<Task>.constant(Task(name: "Test Task", isComplete: false, lastCompleted: nil)))
+                .previewInterfaceOrientation(.portraitUpsideDown)
+            DetailsView(task: Binding<Task>.constant(Task(name: "Test Task", isComplete: false, lastCompleted: nil)))
+                .previewInterfaceOrientation(.landscapeRight)
+        }
     }
 }
